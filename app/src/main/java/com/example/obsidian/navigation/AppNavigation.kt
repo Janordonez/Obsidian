2989bc2b1b2737723d2513bf61d074b90a2bd749
cfbj11 <cfbriones@uamv.edu.ni>
2026-06-22 23:16:51 -0600

    fix: error de ejecución. modificación y solución temporal.

--- app/src/main/java/com/example/obsidian/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/obsidian/navigation/AppNavigation.kt
@@ -57,3 +57,15 @@ fun AppNavigation(
     }
 }
 
+// VerdictScreen y AccusationScreen faltantes por implementar. Modificación temporal.
+
+@Composable
+fun VerdictScreen(x0: NavHostController, x1: GameViewModel, x2: Modifier) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun AccusationScreen(x0: NavHostController, x1: GameViewModel, x2: Modifier) {
+    TODO("Not yet implemented")
+}
+
